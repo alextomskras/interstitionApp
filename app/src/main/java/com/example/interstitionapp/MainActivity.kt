e98fe5fbb5dc01dde9adee623f7ace3e43065a26
fess <dreamer.alex.toms@gmail.com>
2019-10-13 22:30:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/interstitionapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.interstitionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mInterstitialAd: InterstitialAd
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        mInterstitialAd.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+
+                    mInterstitialAd.show()
+                super.onAdLoaded()
+                }
+            }
+
+
+        }
+    }
