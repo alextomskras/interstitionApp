1fba37f5744497396389344deecd2425687601c4
fess <dreamer.alex.toms@gmail.com>
2019-10-16 23:28:47 +0300

    try check override back pressed
    Signed-off-by: fess <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/example/interstitionapp/HelloActivity.kt
+++ app/src/main/java/com/example/interstitionapp/HelloActivity.kt
@@ -1,39 +1,36 @@
 package com.example.interstitionapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var mInterstitialAd: InterstitialAd
+private lateinit var mInterstitialAd: InterstitialAd
+
+class HelloActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hello)
+
 
         MobileAds.initialize(this) {}
         mInterstitialAd = InterstitialAd(this)
         mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
         mInterstitialAd.loadAd(AdRequest.Builder().build())
         mInterstitialAd.adListener = object : AdListener() {
-            override fun onAdLoaded() {
-
-                mInterstitialAd.show()
-                super.onAdLoaded()
-            }
         }
+    }
 
-        b1.setOnClickListener {
-
-            var intent = Intent.EXTRA_INTENT
+    override fun onBackPressed() {
+        if (mInterstitialAd.isLoaded) {
+            mInterstitialAd.show()
         }
-
+        super.onBackPressed()
 
     }
+
 }
